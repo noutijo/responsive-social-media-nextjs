6df223d9c605b19126eedd802dc4d611c2bd3b8d
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-10-09 16:38:43 +0100

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./data/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "Karla", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        colorPrimary: "var(--color-primary)",
+        colorWhite: "var(--color-white)",
+        colorLight: "var(--color-light)",
+        colorGray: "var(--color-gray)",
+        colorSecondary: "var(--color-secondary)",
+        colorSucsess: "var(--color-success)",
+        colorDanger: "var(--color-danger)",
+        colorDark: "var(--color-dark)",
+        colorBlack: "var(--color-black)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
