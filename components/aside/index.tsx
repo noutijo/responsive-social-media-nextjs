e6bc23b0e53a9fd4d49d29ef21198650dab196ee
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-10-11 03:39:23 +0100

    add drawer hook

--- components/aside/index.tsx
+++ components/aside/index.tsx
@@ -17,7 +17,7 @@ export default function Aside() {
   return (
     <>
       <aside
-        className={`fixed h-screen bg-colorLight top-0 left-0 p-4 w-[84vw] land sm:landscape:w-[47vw] overflow-y-auto  md:landscape:w-max lg:overflow-y-hidden md:w-max md:bg-transparent md:p-0 md:h-max z-[20] md:sticky md:top-[6.5rem] md:block ${
+        className={`fixed h-screen bg-colorLight top-0 p-4 w-[84vw] land sm:landscape:w-[47vw] overflow-y-auto  md:landscape:w-max !left-[-0%] md:left-[0%] animate-showmenu md:animate-none lg:overflow-y-hidden md:w-max md:bg-transparent md:p-0 md:h-max z-[10] md:sticky md:top-[6.5rem] md:block ${
           drawer.isopen ? "block" : "hidden"
         }`}>
         <div className="p-4 flex bg-colorWhite rounded-2xl items-center gap-4 w-full">
@@ -28,9 +28,11 @@ export default function Aside() {
             <h3 className="text-md font-medium">Oreol Noumodong</h3>
             <p className="text-colorGray text-sm">@oreol</p>
           </div>
-          <button className="lg:hidden w-[3rem] h-[3rem] absolute top-50 right-6" onClick={hideAsideBarMenu}>
-          <i className="bi bi-x text-3xl"> </i>
-        </button>
+          <button
+            className="md:hidden   w-[3rem] h-[3rem] absolute top-50 right-6"
+            onClick={hideAsideBarMenu}>
+            <i className="bi bi-x text-3xl"> </i>
+          </button>
         </div>
         <div className="mt-4 bg-colorWhite rounded-2xl">
           {/** get all menu items and display*/}
@@ -45,7 +47,7 @@ export default function Aside() {
           ))}
         </div>
 
-        <div className="md:hidden lg:block mt-4 w-full text-center bg-colorPrimary text-white px-6 py-3 rounded-full cursor-pointer font-medium transition duration-30 ease-out inline-block text-sm">
+        <div className="hidden lg:block mt-4 w-full text-center bg-colorPrimary text-white px-6 py-3 rounded-full cursor-pointer font-medium transition duration-30 ease-out inline-block text-sm">
           <label className="cursor-pointer" htmlFor="create-post">
             Create Post
           </label>
