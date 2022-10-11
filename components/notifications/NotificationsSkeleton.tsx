61793e198754e1c8212d34e68b6145ac89919b49
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-10-11 05:09:03 +0100

    start notifications fnc

---
+++ components/notifications/NotificationsSkeleton.tsx
@@ -0,0 +1,20 @@
+export default function NotificationsSkeleton() {
+  return (
+    <>
+      {/** get all posts/feeds and display*/}
+      {["", "", "", "", ""].map((item, __) => (
+        <div key={__} className="bg-colorWhite rounded-md p-4 w-full mx-auto">
+          <div className="animate-pulse flex space-x-4 items-center">
+            <div className="rounded-full bg-colorLight h-10 w-10"></div>
+            <div className="flex-1 space-y-4 ">
+              <div className="h-2 bg-colorLight rounded"></div>
+              <div className="space-y-3">
+                <div className="h-2 bg-colorLight rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- components/notifications/index.tsx
+++ components/notifications/index.tsx
@@ -1,18 +1,45 @@
-import Image from "next/image";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import NotificationItem from "./NotificationItem";
+import NotificationsSkeleton from "./NotificationsSkeleton";
 
 export default function AllNotifications() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let reqOptions = {
+      url: "https://jsonplaceholder.typicode.com/photos",
+      method: "GET",
+    };
+
+    setLoading(true);
+    let data = await axios.request(reqOptions);
+    setLoading(false);
+    console.log(data);
+  };
+
   return (
     <>
       <div className="w-full">
         <h2 className="text-md font-medium">All notifications</h2>
-       <div className="bg-colorWhite rounded-2xl p-4 mt-2">
-        <NotificationItem />
-        <NotificationItem />
-        <NotificationItem />
-        <NotificationItem />
-        <NotificationItem />
-      </div>
+
+        <div className="bg-colorWhite rounded-2xl p-4 mt-2">
+          {loading ? (
+            <NotificationsSkeleton />
+          ) : (
+            <>
+              <NotificationItem />
+              <NotificationItem />
+              <NotificationItem />
+              <NotificationItem />
+              <NotificationItem />
+            </>
+          )}
+        </div>
       </div>
     </>
   );
