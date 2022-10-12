69adcae25ac931928c80be883bf1e817300517a0
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-10-12 04:51:05 +0100

    Refactoring

--- components/theme/ThemePrimaryColor.tsx
+++ components/theme/ThemePrimaryColor.tsx
@@ -19,7 +19,7 @@ export default function ThemePrimaryColor({
         }}
         className={`${primaryColorClassName} w-8 h-8 ${
           currentPrimaryColor === primaryColorClassName
-            ? "border-4 bg-colorGray"
+            ? "border-4 border-white"
             : ""
         } rounded-full cursor-pointer`}
         onClick={() => setPrimaryColor(primaryColorClassName)}></span>
